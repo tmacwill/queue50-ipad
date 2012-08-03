e68c1047a5553b72a0123c159895138f8616077b
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2012-08-03 19:03:47 -0400

    updated label colors for new color scheme

--- CS50Help/RootViewController.h
+++ CS50Help/RootViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
+// OMG BEST MARCRO EVER http://cocoamatic.blogspot.com/2010/07/uicolor-macro-with-hex-values.html
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+
 @class HalfViewController;
 @class Question;
 @class TF;
